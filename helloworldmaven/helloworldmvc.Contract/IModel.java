be3dda4a00b5ab8988e9f4aeada52d83e3a53aec
Luc Antoni <l.antoni@hotmail.fr>
2019-05-07 09:46:01 +0200

    Phase 2

---
+++ helloworldmaven/helloworldmvc.Contract/IModel.java
@@ -0,0 +1,5 @@
+package fr.exia;
+
+public interface IModel {
+
+}
---
+++ helloworldmaven/helloworldmvc.Contract/Model.java
@@ -0,0 +1,7 @@
+package fr.exia;
+
+public class Model implements IModel{
+	public String getHelloWorld() {
+		return null;
+	}
+}
