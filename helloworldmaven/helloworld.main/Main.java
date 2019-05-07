85284defaec94ed738c5f10557b2f5446482b5e6
Luc Antoni <l.antoni@hotmail.fr>
2019-05-07 11:26:45 +0200

    fin prématurée

---
+++ helloworldmaven/helloworld.main/Main.java
@@ -0,0 +1,14 @@
+package fr.exia;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*IView view;
+		IModel model;
+		Controller c = new Controller(view, model);
+		c.run();*/
+		System.out.println("lol");
+	}
+
+}
